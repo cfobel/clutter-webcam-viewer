eb80e69272427d1cd32419001fefdccf6af127bc
Christian Fobel <christian@fobel.net>
2015-05-22 13:22:22 -0400

    Initial commit of Clutter/GTK3/GStreamer 1.0 demo

---
+++ setup.py
@@ -0,0 +1,17 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+import sys
+sys.path.insert(0, '.')
+import version
+
+
+setup(name='clutter-webcam-viewer',
+      version=version.getVersion(),
+      author='Christian Fobel',
+      author_email='christian@fobel.net',
+      url='https://github.com/cfobel/clutter-webcam-viewer',
+      license='LGPL-3.0',
+      packages=['clutter_webcam_viewer'])
